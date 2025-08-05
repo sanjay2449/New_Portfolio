022d5bc3a5b736e7437f4869f03bd4567c903a3d
Sanjay Chourasiya <sanjaychourasiyadmo240999@gmail.com>
2025-08-05 16:31:53 +0530

    dv

--- src/sections/ResumeSection.jsx
+++ src/sections/ResumeSection.jsx
@@ -80,9 +80,9 @@ const ResumeSection = () => {
 
         {/* projects */}
         <motion.div
-          initial={{ opacity: 0, y: 40 }}
+          initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
+          transition={{ duration: 0.5, delay: 0.1 }}
           className="py-18 mt-20"
           id="projects"
         >
